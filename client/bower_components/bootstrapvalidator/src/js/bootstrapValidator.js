6991cf9c4114ceb312231984f063ff8942b52078
Robin Temme <robin@n3rdkeller.de>
2014-10-26 20:01:47 +0100

    Add BootstrapValidator

--- client/bower_components/bootstrapvalidator/src/js/bootstrapValidator.js
+++ client/bower_components/bootstrapvalidator/src/js/bootstrapValidator.js
@@ -304,7 +304,7 @@ if (typeof jQuery === 'undefined') {
                 $field.off(events).on(events, function() {
                     that.updateStatus($(this), that.STATUS_NOT_VALIDATED);
                 });
-                
+
                 // Create help block elements for showing the error messages
                 $field.data('bv.messages', $message);
                 for (validatorName in this.options.fields[field].validators) {
@@ -364,7 +364,7 @@ if (typeof jQuery === 'undefined') {
                         $icon.addClass('bv-icon-input-group')
                              .insertAfter($parent.find('.input-group').eq(0));
                     }
-                    
+
                     if (container) {
                         $field
                             // Show tooltip/popover message when field gets focus
@@ -593,7 +593,7 @@ if (typeof jQuery === 'undefined') {
             var cannotType = $.inArray($field.attr('type'), ['button', 'checkbox', 'file', 'hidden', 'image', 'radio', 'reset', 'submit']) !== -1;
             return (cannotType || $field.val().length >= threshold);
         },
-        
+
         // ---
         // Events
         // ---
@@ -947,7 +947,7 @@ if (typeof jQuery === 'undefined') {
                 $(this).data('bv.messages').find('.help-block[data-bv-validator="' + validator + '"][data-bv-for="' + field + '"]').html(message);
             });
         },
-        
+
         /**
          * Update all validating results of field
          *
@@ -1692,7 +1692,7 @@ if (typeof jQuery === 'undefined') {
         // - a CSS selector indicating the container
         // In the first two cases, since the tooltip/popover should be small enough, the plugin only shows only one error message
         // You also can define the message container for particular field
-        container: null,
+        container: popover,
 
         // The field will not be live validated if its length is less than this number of characters
         threshold: null,
@@ -1775,7 +1775,7 @@ if (typeof jQuery === 'undefined') {
             validatorError: 'error.validator.bv',
             validatorSuccess: 'success.validator.bv'
         },
-        
+
         // Whether to be verbose when validating a field or not.
         // Possible values:
         // - true:  when a field has multiple validators, all of them will be checked, and respectively - if errors occur in
