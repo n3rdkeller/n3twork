60c62decaf5b7f73d79a94b94a0d21664351badc
Johannes Wolf <johannes@n3rdkeller.de>
2014-10-26 23:28:16 +0100

    Add all (empty) methods and attributes of Group as specified in UML-Diagram

--- server/src/classes/Group.java
+++ server/src/classes/Group.java
@@ -1,5 +1,56 @@
 package classes;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import calsses.*;
 
 public class Group {
+  private int id;
+  private String name;
+  private String descr;
+  private List<User> members = new ArrayList<User>()
+  private User owner;
+  private Map<String,String> otherProperties = new HashMap<String,String>();
+
+  public String getName() {
+    return null;
+  }
+
+  public String getDescr() {
+    return null;
+  }
+
+  public void setDescr(String descr) {
+
+  }
+
+  public User getOwner() {
+    return null;
+  }
+
+  private void setOwner() {
+
+  }
+
+  public List<User> getUsers() {
+    return null;
+  }
+
+  public void addUser(User user) {
+
+  }
+
+  public void removeUser(User user) {
+
+  }
+
+  public String getOtherProperty(String key) {
+    return null;
+  }
+
+  public void setOtherProperty(String key, String value) {
+
+  }
 
 }
