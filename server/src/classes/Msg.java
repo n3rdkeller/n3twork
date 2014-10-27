af4a6a0ef82f3e17a7af8e1d447388a43574bdab
Johannes Wolf <johannes@n3rdkeller.de>
2014-10-27 11:28:23 +0100

    Add all (empty) methods and attributes of Msg as specified in UML-diagram

--- server/src/classes/Msg.java
+++ server/src/classes/Msg.java
@@ -1,5 +1,38 @@
 package classes;
 
 public class Msg {
+  private int id;
+  private User sender;
+  private List<User> reciever = new ArrayList<User>();
+  private String subject;
+  private Date sendDate;
+  private Map<User,Boolean> read = new HashMap<User,Boolean>();
 
+  public User getSender() {
+    return null;
+  }
+
+  public List<User> getReciever() {
+    return null;
+  }
+
+  public String getSubject() {
+    return null;
+  }
+
+  public String getContent() {
+    return null;
+  }
+
+  public Date getSendDate() {
+    return null;
+  }
+
+  public Map<User,Boolean> getRead() {
+    return null;
+  }
+
+  public void setRead(User user) {
+
+  }
 }
