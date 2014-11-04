eb5a160afa8fc840f884d489ad2e663b9642e633
Johannes Wolf <johannes@n3rdkeller.de>
2014-11-04 11:58:21 +0100

    Add shell scripts to compile and to run

---
+++ server/compile.sh
@@ -0,0 +1,1 @@
+javac -sourcepath src -classpath bin/classes/dep/mysql-connector-java-5.1.33-bin.jar src/classes/*.java -d bin/classes
---
+++ server/run.sh
@@ -0,0 +1,1 @@
+java -cp .:src/dep/mysql-connector-java-5.1.33-bin.jar:bin/classes classes.Main
