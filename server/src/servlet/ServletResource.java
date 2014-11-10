754bd132b7a484df6eb759519eae3a6cd12bc2d0
Robin Temme <robin@n3rdkeller.de>
2014-11-10 16:05:20 +0100

    Add Response header to get it to work with client

--- server/src/servlet/ServletResource.java
+++ server/src/servlet/ServletResource.java
@@ -4,14 +4,17 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/")
 public class ServletResource {
 
   @GET
   @Produces(MediaType.TEXT_PLAIN)
-  public String sayHello() {
-    return "Hello World!";
+  public Response sayHello() {
+    return Response.ok("You")
+    		.header("Access-Control-Allow-Origin", "*")
+    		.build();
   }
 
   /*@GET @Path("/bye")
