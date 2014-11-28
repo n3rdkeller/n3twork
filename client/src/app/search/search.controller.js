c4ea54e4a63e6ba2bc5a5a037fa8466f43cf8301
Robin Temme <robin@n3rdkeller.de>
2014-11-28 03:29:28 +0100

    Add search controller logic

--- client/src/app/search/search.controller.js
+++ client/src/app/search/search.controller.js
@@ -9,7 +9,44 @@
   function SearchCtrl(SearchSvc) {
     var vm = this;
 
+    // functions
+    vm.loadSearchList = loadSearchList;
+    vm.loadUserList = loadUserList;
 
+    function loadSearchList(valid, searchString) {
+      vm.firstLoading = true;
+      loadUserList(searchString);
+      vm.secondLoading = true;
+      loadGroupList(searchString);
+      vm.searchString = "";
+    }
 
+    function loadUserList(searchString) {
+      if (searchString) {
+        if (searchString.length > 2) {
+          SearchSvc.getUserList().then(function (response) {
+            vm.userlist = response.userList;
+            vm.submittedSearchString = searchString;
+            vm.firstLoading = false;
+          });
+        } else {
+          vm.userlist = [];
+          vm.submittedSearchString = searchString;
+          vm.firstLoading = false;
+        }
+      } else {
+        vm.userlist = [];
+        vm.submittedSearchString = searchString;
+        vm.firstLoading = false;
+      }
+    }
+
+    function loadGroupList(searchString) {
+      SearchSvc.getGroupList().then(function (response) {
+        vm.grouplist = response.groupList;
+        vm.submittedSearchString = searchString;
+        vm.secondLoading = false;
+      });
+    }
   }
 })();
