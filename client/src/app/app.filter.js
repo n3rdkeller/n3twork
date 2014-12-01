983bc0dc1f35065f41ebcc8145e9290249f3e016
Robin Temme <robin@n3rdkeller.de>
2014-12-01 04:11:19 +0100

    Add some filters

---
+++ client/src/app/app.filter.js
@@ -0,0 +1,55 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('n3twork')
+    .filter('capitalize', capitalize);
+
+    function capitalize() {
+      return function (input, all) {
+        return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
+      };
+    }
+})();
+
+(function() {
+  'use strict';
+
+  angular
+    .module('n3twork')
+    .filter('symbolForKey', symbolForKey);
+
+    function symbolForKey() {
+      return function (input) {
+        var symbol = '';
+        switch (input) {
+          case 'city':
+            symbol = 'location-arrow';
+            break;
+          default:
+            symbol = 'cog';
+            break;
+        }
+        return symbol;
+      };
+    }
+})();
+
+(function() {
+  'use strict';
+
+  angular
+    .module('n3twork')
+    .filter('isEmpty', isEmpty);
+
+    function isEmpty() {
+      return function (object) {
+        for (var key in object) {
+          if (object.hasOwnProperty(key)) {
+            return false;
+          }
+        }
+        return true;
+      };
+    }
+})();
