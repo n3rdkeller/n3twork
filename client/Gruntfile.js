a6af50d083788ad7fac1c0f878ce542e44f23d67
Robin Temme <robin@n3rdkeller.de>
2014-12-01 04:12:50 +0100

    Minor change in gruntfile.js

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function(grunt) {
         options: {
           sourceMap: false
         },
-        src: ['src/js/angular.js', 'src/js/jquery.js', 'src/js/*.js', 'src/app/app.js', 'src/app/*/*.js'],
+        src: ['src/js/angular.js', 'src/js/jquery.js', 'src/js/*.js', 'src/app/app.js', 'src/app/app.*.js', 'src/app/*/*.js'],
         dest: 'dist/js/n3twork.min.js'
       }
     },
