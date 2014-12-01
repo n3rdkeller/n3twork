45577027413242dcad0167bef8816fb07d56f5b3
Johannes Wolf <johannes@n3rdkeller.de>
2014-12-01 01:40:15 +0100

    Fixes: otherProperties as json and showing profil with username
    If any key of otherProperties is empty, it won't be put in the json object "otherProperties"
    It is now possible to get all friends and groups of a user by only knowing the username

--- server/src/classes/User.java
+++ server/src/classes/User.java
@@ -836,7 +836,8 @@ public class User {
         "SELECT Users.id,username,email,name,firstName,Friends.date FROM " + DBConnector.DATABASE + ".Friends "
         + "JOIN " + DBConnector.DATABASE + ".Users "
         + "ON Users.id=Friends.friendID "
-        + "WHERE Friends.userID=" + this.id;
+        + "WHERE Users.id=" + this.id
+        + "OR Users.username='" + this.username + "'";
     PreparedStatement pStmt = conn.prepareStatement(sqlQuery);
     ResultSet friendsTable = pStmt.executeQuery();
     log.debug(sqlQuery);
