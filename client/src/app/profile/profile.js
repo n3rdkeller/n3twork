cd87a8c3ede5ff9eb70db4aeda293b162be7751a
Robin Temme <robin@n3rdkeller.de>
2015-01-03 16:50:44 +0100

    Paste comment-implementation from feed.js

--- client/src/app/profile/profile.js
+++ client/src/app/profile/profile.js
@@ -17,16 +17,33 @@
   function ProfileCtrl(APISvc, CacheSvc, PostSvc, VoteSvc, $q, $rootScope, $routeParams) {
     var vm = this;
 
+    // friends
     vm.friendAction = friendAction;
+    // post
     vm.newPost = newPost;
     vm.removePost = removePost;
+    // vote
     vm.showVotes = showVotes;
     vm.voteAction = voteAction;
+    // comment
+    vm.commentAction = commentAction;
+    vm.newComment = newComment;
+    vm.removeComment = removeComment;
 
+    // post
     vm.newPostPrivate = false;
     vm.removePostButtonLoading = {};
     vm.removeButtonConfirmation = {};
+    // vote
     vm.voteButtonLoading = {};
+    // comments
+    vm.commentsLoading = {};
+    vm.showComments = {};
+    vm.newCommentForm = {};
+    vm.newCommentText = {};
+    vm.newCommentLoading = {};
+    vm.removeCommentButtonConfirmation = {};
+    vm.removeCommentButtonLoading = {};
 
     init();
 
@@ -193,6 +210,68 @@
         // error
       });
 
+    }
+
+    function commentAction (postID, numberOfComments) {
+      vm.showComments[postID] = !vm.showComments[postID];
+      if (numberOfComments != 0) {
+        vm.commentsLoading[postID] = true;
+        if (vm.showComments[postID]) {
+          CommentSvc.getCommentList(postID).then(function (commentList) {
+              vm.commentsLoading[postID] = false;
+              addCommentsToPost(commentList, postID);
+            }, function (error) {
+              vm.commentsLoading[postID] = false;
+            });
+        } else {
+          vm.commentsLoading[postID] = false;
+        }
+      }
+    }
+
+    function addCommentsToPost (commentList, postID) {
+      for (var i = 0; i < vm.postlist.length; i++) {
+        if (vm.postlist[i].id == postID) {
+          vm.postlist[i].comments = commentList;
+          vm.postlist[i].numberOfComments = commentList.length;
+        }
+      };
+    }
+
+    function newComment (postID) {
+      vm.newCommentLoading[postID] = true;
+      CommentSvc.newComment(postID, vm.newCommentText[postID]).then(function (successful) {
+        CommentSvc.getCommentList(postID).then(function (commentList) {
+            addCommentsToPost(commentList, postID);
+            vm.newCommentLoading[postID] = false;
+            resetNewCommentForm(postID);
+          }, function (error) {
+            vm.newCommentLoading[postID] = false;
+          });
+      }, function (error) {
+        vm.newCommentLoading[postID] = false;
+      });
+    }
+
+    function resetNewCommentForm (postID) {
+      vm.newCommentText[postID] = "";
+    }
+
+    function removeComment (commentID, postID) {
+      vm.removeCommentButtonLoading[commentID] = true;
+      CommentSvc.removeComment(commentID, postID).then(function (successful) {
+        CommentSvc.getCommentList(postID).then(function (commentList) {
+          addCommentsToPost(commentList, postID);
+          vm.removeCommentButtonConfirmation[commentID] = false;
+          vm.removeCommentButtonLoading[commentID] = false;
+        }, function (error) {
+          vm.removeCommentButtonLoading[commentID] = false;
+        });
+      }, function (error) {
+        // error deleting the comment
+        vm.removeCommentButtonConfirmation[commentID] = false;
+        vm.removeCommentButtonLoading[commentID] = false;
+      });
     }
 
   }
