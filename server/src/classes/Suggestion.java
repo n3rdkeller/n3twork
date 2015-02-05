adcdd29ace6861e72f9f7d0505f9f55c2cbf1a3c
Johannes Wolf <johannes@n3rdkeller.de>
2015-02-05 12:58:34 +0100

    Continue with algo
    count words and calculate wij of every entry of the matrix

--- server/src/classes/Suggestion.java
+++ server/src/classes/Suggestion.java
@@ -3,6 +3,7 @@
  */
 package classes;
 
+import gnu.trove.TDoubleArrayList;
 import gnu.trove.TIntArrayList;
 import gnu.trove.TIntDoubleHashMap;
 
@@ -127,6 +128,7 @@ public class Suggestion {
   
   public static List<User> postBasedSuggestion(User user) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
     Connection conn = DBConnector.getConnection();
+    // select the user in question and all users, that are not a friend of them
     String sql = "SELECT Posts.content, Users.id, Users.username, Users.email, Users.name, Users.firstName "
         + "FROM " + DBConnector.DATABASE + ".Posts "
         + "JOIN " + DBConnector.DATABASE + ".Users ON Posts.authorID = Users.id "
@@ -139,25 +141,105 @@ public class Suggestion {
     pStmt.setInt(1, user.getId());
     pStmt.setInt(2, user.getId());
     ResultSet postTable = pStmt.executeQuery();
-    
+    // put postTable in Hashmap<User, ArrayList<String>>, where every word in the posts is an entry in the ArrayList<String>
     User author = new User();
     Map<User,ArrayList<String>> wordTable = new HashMap<User,ArrayList<String>>();
     ArrayList<String> wordRow = new ArrayList<String>();
     while(postTable.next()) {
+      //if the author is new, then add the old one to the HashMap
       if(author.getId() != postTable.getInt("id")) {
         if(wordRow.size() != 0) {
+          Collections.sort(wordRow);
           wordTable.put(author, wordRow);
         }
         wordRow = new ArrayList<String>();
-        user = new User(
+        author = new User(
             postTable.getInt("id"),
             postTable.getString("username"),
             postTable.getString("email"),
             postTable.getString("name"),
             postTable.getString("firstName"));
       }
+      //split the post into words and add them to wordRow
       for(String word: postTable.getString("content").split("[0123456789 +-=.,!?@#$%^&*();\\/|<>'\"\t]")) {
         wordRow.add(word.toLowerCase());
+      }
+    }
+    //add last author to the wordTable
+    Collections.sort(wordRow);
+    wordTable.put(author, wordRow);
+    
+    //fill the matrix
+    List<String> wordList = new ArrayList<String>();
+    List<TDoubleArrayList> wordUserMatrix = new ArrayList<TDoubleArrayList>();
+    TDoubleArrayList matrixRow = new TDoubleArrayList();
+    for(Entry<User, ArrayList<String>> row: wordTable.entrySet()) {
+      matrixRow.add(row.getKey().getId());
+      int wordCounter = 0;
+      //count duplicates of every word in the row, which is already in the wordList
+      //and save that in the matrixRow
+      for(String headerWord: wordList) {
+        int i = row.getValue().indexOf(headerWord);
+        wordCounter = 0;
+        if(i != -1) {
+          while(row.getValue().get(i) == headerWord) {
+            wordCounter++;
+            i++;
+          }
+        }
+        matrixRow.add(wordCounter);
+      }
+      
+      //go through the entire row and check for every new word if it is already in the wordList.
+      //if it is not, then add it to the wordList and count, how often it occurs in the row.
+      Boolean addWord = false;
+      String lastWord = "";
+      for(String word: row.getValue()) {
+        if(word == lastWord) {
+          if(addWord) {
+            wordCounter++;
+          }
+          continue;
+        }
+        if(!wordList.contains(word)) {
+          if (addWord) {
+            matrixRow.add(wordCounter);
+            wordCounter = 1;
+          }
+          wordList.add(word);
+          addWord = true;
+        } else {
+          addWord = false;
+        }
+        lastWord = word;
+      }
+      wordUserMatrix.add(matrixRow);
+    }
+    //the bigger the row index of the matrix the longer the row!
+    //first entry of row is the id of the user!
+    TIntArrayList nList = new TIntArrayList(wordList.size());
+    for(int i = 1; i <= wordList.size(); i++) {
+      int n = 0;
+      for(TDoubleArrayList row: wordUserMatrix){
+        if(row.size() >= i && row.get(i) != 0) { 
+          n++;
+        }
+      }
+      nList.add(n);
+    }
+    TDoubleArrayList maxList = new TDoubleArrayList(wordUserMatrix.size());
+    for(TDoubleArrayList row: wordUserMatrix) {
+      maxList.add(row.max());
+    }
+    for(int i = 1; i <= wordList.size(); i++) {
+      int j = 0;
+      for(TDoubleArrayList row: wordUserMatrix) {
+        if(row.size() >= i && row.get(i) != 0) {
+          // calculation of wij = (hij/maxk hkj)*log(N/ni)
+          Double newEntry = (row.get(i)/maxList.get(j))*Math.log(wordUserMatrix.size()/nList.get(i));
+          row.set(i, newEntry);
+        }
+        j++;
       }
     }
     return null;
